6cec401f91268762cb0a77f8429b25a0885c6c7b
Becky Nisttahuz <benistta@hotmail.com>
2022-09-15 07:54:56 -0600

    Feat:Change factories created

---
+++ spec/factories/subscription.rb
@@ -0,0 +1,10 @@
+# FactoryBot.define do
+#   factory :subscription do
+#     customer
+#     tea
+#     title { "My #{Faker::Tea.variety} Subscription" }
+#     price { Faker::Number.decimal(r_digits: 2) }
+#     status { [0, 1].sample }
+#     frequency { [0, 1, 2].sample }
+#   end
+# end
