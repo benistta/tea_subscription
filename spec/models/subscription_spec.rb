030c92eb0c0fb705aa3c53000497260386af64b3
Becky Nisttahuz <benistta@hotmail.com>
2022-09-15 16:41:25 -0600

    Update:requests tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Subscription, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :price }
-    it { should validate_numericality_of :price }
     it { should validate_presence_of :status }
     it { should validate_presence_of :frequency }
   end
