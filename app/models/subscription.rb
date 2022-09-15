b4ed72e9b00d659f203b9d9b36dd8406b1c587d5
Becky Nisttahuz <benistta@hotmail.com>
2022-09-15 10:06:39 -0600

    Update:subscriptions model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,8 @@ class Subscription < ApplicationRecord
 
   validates_presence_of :title, :price, :status, :frequency
 
-  enum status: { cancelled: 0, active: 1 }
-  enum frequency: { weekly: 0, monthly: 1 }
+  # enum status: { cancelled: 0, active: 1 }
+  # enum frequency: { weekly: 0, monthly: 1 }
+  # enum status: [:active, :canceled]
+  # enum frequency: [:weekly, :monthly, :annually]
 end
